8db35faff69535b5ba6d152273ed8a9204e0b631
Guilherme Gomes <guilherme.gomes6875@gmail.com>
2023-01-22 20:47:34 -0300

    Update

---
+++ Basic/4-Vector.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int vector[5] = {1,2,3,4,5};
+
+    printf("Valores de um vetor: ");
+
+    for (int i=0; i < 5; i++) {
+        printf("%i ", vector[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ Basic/8-SchoolReport.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int grade[5] = {0};
+
+    for (int i = 0; i < 5; i++)
+    {
+        int value;
+        printf("Digite a nota do aluno: ");
+        scanf("%i", &value);
+        grade[i] = value;
+    }
+
+    for (int j = 0; j < 5; j++)
+    {
+        printf("%i ", grade[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
