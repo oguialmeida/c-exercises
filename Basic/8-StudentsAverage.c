28d1e97a43a71cb00771f42b65f83ba6b8ee2f69
Guilherme Gomes <guilherme.gomes6875@gmail.com>
2023-01-24 18:19:51 -0300

    Arrays Update

---
+++ Basic/8-StudentsAverage.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    system("clear");
+
+    float grades[4][4] = {0};
+    float studentsAverage[4] = {0};
+    float average = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Insira 4 notas:\n");
+        for (int j = 0; j < 4; j++)
+        {
+            scanf("%f", &grades[i][j]);
+            average += grades[i][j];
+        }
+        studentsAverage[i] = average / 4;
+        average = 0;
+    }
+    for (int k = 0; k < 4; k++)
+    {
+        printf("\nMédia dos aluno %i é %.2f\n", k, studentsAverage[k]);
+    }
+
+    return 0;
+}
